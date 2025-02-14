2ff2dabb30aded5aa81d463e0f206301c393369e
roxanmlr <roxanmlr@gmail.com>
2025-02-14 15:34:03 +0100

    Home as Text testing page

--- mobile/lib/pages/HomePage.dart
+++ mobile/lib/pages/HomePage.dart
@@ -10,7 +10,27 @@ class HomePage extends StatelessWidget {
       body: SizedBox(
         height: size.height,
         width: size.width,
-        child: null,
+        child: Column(
+          children: [
+            Text("headline Large",
+                style: Theme.of(context).textTheme.headlineLarge),
+            Text("headline Medium",
+                style: Theme.of(context).textTheme.headlineMedium),
+            Text("headline Small",
+                style: Theme.of(context).textTheme.headlineSmall),
+            Text("Title Large", style: Theme.of(context).textTheme.titleLarge),
+            Text("Title Medium",
+                style: Theme.of(context).textTheme.titleMedium),
+            Text("Title Small", style: Theme.of(context).textTheme.titleSmall),
+            Text("body Large", style: Theme.of(context).textTheme.bodyLarge),
+            Text("body Medium", style: Theme.of(context).textTheme.bodyMedium),
+            Text("body Small", style: Theme.of(context).textTheme.bodySmall),
+            Text("label Large", style: Theme.of(context).textTheme.labelLarge),
+            Text("label Medium",
+                style: Theme.of(context).textTheme.labelMedium),
+            Text("label Small", style: Theme.of(context).textTheme.labelSmall),
+          ],
+        ),
       ),
     );
   }
