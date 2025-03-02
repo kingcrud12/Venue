44a0ce8908a152cae95539fd304f6f9cc1e72a6f
roxanmlr <roxanmlr@gmail.com>
2025-03-02 11:11:38 +0100

    Database local connection on echo server

--- echo/src/app.module.ts
+++ echo/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EchoController } from './echo/echo.controller';
 import { EchoService } from './echo/echo.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule,
+	   TypeOrmModule.forRoot({
+		type: 'postgres',
+		host: 'localhost',
+		port: 5332,
+		username: 'database_admin',
+		password: process.env.DB_PASSWORD,
+		database: 'venue_test',
+		autoLoadEntities: true,
+		synchronize: true})
+	   ],
   controllers: [AppController, EchoController],
   providers: [AppService, EchoService],
 })
