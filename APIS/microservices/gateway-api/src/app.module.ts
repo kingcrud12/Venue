9f83560d0168ee9b576e5f41caabf91de9be67a9
kingcrud12 <dipitay@gmail.com>
2025-03-02 14:29:22 +0100

    adding database config

--- APIS/microservices/gateway-api/src/app.module.ts
+++ APIS/microservices/gateway-api/src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.AUTH_SERVICE_HOST ?? 'localhost',
+          port: parseInt(process.env.AUTH_SERVICE_PORT ?? '3001'),
+        },
+      },
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.USER_SERVICE_HOST ?? 'localhost',
+          port: parseInt(process.env.USER_SERVICE_PORT ?? '3002'),
+        },
+      },
+      {
+        name: 'POST_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.POST_SERVICE_HOST ?? 'localhost',
+          port: parseInt(process.env.PRODUCT_SERVICE_PORT ?? '3003'),
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
